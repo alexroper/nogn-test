0c6c046f4603d21a4332d3a689e91deef68f34ba
Alex Roper <alex.roper@gmail.com>
2021-02-19 16:08:03 -0800

    Create Test template in Gridsome

---
+++ gridsome/src/pages/Test.vue
@@ -0,0 +1,19 @@
+<template>
+  <Layout>
+    <h2>Testing Nogn</h2>
+    <pre>
+      #{{ $page.craft.entry.id }}: {{ $page.craft.entry.title }}
+    </pre>
+  </Layout>
+</template>
+
+<page-query>
+  query {
+    craft {
+      entry (section: "test") {
+        id
+        title
+      }
+    }
+  }
+</page-query>
